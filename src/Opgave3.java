8a1e3519b0345bed6acb3eeb33b12b3fb3449d45
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-17 10:30:26 +0200

    Test cases opstillet

---
+++ src/Opgave3.java
@@ -0,0 +1,22 @@
+public class Opgave3 {
+    /*Lav en metode midterst, der tager tre tal som parametre.
+Metoden skal returnere den midterste værdi.
+F.eks. skal kaldet midterst( 6, 10, 8 ) returnere 8.
+ */
+    public static void main(String[] args) {
+        //Test case sidste værdi er midterst
+        // input : a = 6, b = 10, c = 8
+        // forventet output = 8
+
+        //test case hvor første værdi er midterst
+        //input: a = 3, b = 7, c = 2
+        // forventet output = 3
+
+        //Test case hvor midterste værdig er midterst
+        // imput: a = 10, b= 25, c= 50
+        //forventet output 25
+
+
+    }
+
+}
